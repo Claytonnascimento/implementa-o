58bea635f6dfea1575b290c00652a4119c13726b
Claytonnascimento <ra202512500@univassouras.edu.br>
2025-11-09 23:42:59 -0300

    Quick Sort (ordenação rápida).py

---
+++ Quick Sort (ordenação rápida).py
@@ -0,0 +1,13 @@
+valores = [29, 10, 14, 37, 14, 3, 76, 45, 20]
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivo = arr[len(arr) // 2]
+        menores = [x for x in arr if x < pivo]
+        iguais = [x for x in arr if x == pivo]
+        maiores = [x for x in arr if x > pivo]
+        return quick_sort(menores) + iguais + quick_sort(maiores)
+
+print("Quick Sort:", quick_sort(valores.copy()))
