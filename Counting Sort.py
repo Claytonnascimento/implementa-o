7a6b5c77d3c528f6ec601a7cf318769e8d907688
Claytonnascimento <ra202512500@univassouras.edu.br>
2025-11-09 23:48:09 -0300

    Counting Sort.py

---
+++ Counting Sort.py
@@ -0,0 +1,12 @@
+def counting_sort(arr):
+    max_val = max(arr)
+    count = [0] * (max_val + 1)
+    for num in arr:
+        count[num] += 1
+
+    arr_ordenado = []
+    for i in range(len(count)):
+        arr_ordenado.extend([i] * count[i])
+    return arr_ordenado
+    
+print("Counting Sort:", counting_sort([4, 2, 2, 8, 3, 3, 1]))
