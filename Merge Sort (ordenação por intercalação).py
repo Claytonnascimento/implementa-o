cd10f2207607761ee58e05b230f90afaefcd9694
Claytonnascimento <ra202512500@univassouras.edu.br>
2025-11-09 23:41:06 -0300

    Merge Sort (ordenação por intercalação).py
    Implement merge sort algorithm to sort a list of values.

---
+++ Merge Sort (ordenação por intercalação).py
@@ -0,0 +1,33 @@
+valores = [29, 10, 14, 37, 14, 3, 76, 45, 20]
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        meio = len(arr) // 2
+        esquerda = arr[:meio]
+        direita = arr[meio:]
+
+        merge_sort(esquerda)
+        merge_sort(direita)
+
+        i = j = k = 0
+        while i < len(esquerda) and j < len(direita):
+            if esquerda[i] < direita[j]:
+                arr[k] = esquerda[i]
+                i += 1
+            else:
+                arr[k] = direita[j]
+                j += 1
+            k += 1
+
+        while i < len(esquerda):
+            arr[k] = esquerda[i]
+            i += 1
+            k += 1
+
+        while j < len(direita):
+            arr[k] = direita[j]
+            j += 1
+            k += 1
+    return arr
+
+print("Merge Sort:", merge_sort(valores.copy()))
